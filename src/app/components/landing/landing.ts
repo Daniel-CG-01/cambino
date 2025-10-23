5de152fdca9799bba976f1f2d5e6d2b9157e8aa2
DanielZR05 <danielzanon2005@gmail.com>
2025-10-23 12:01:12 +0200

    Primeros pasos

---
+++ src/app/components/landing/landing.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Home } from '../home/home';
+
+@Component({
+  selector: 'app-landing',
+  imports: [],
+  templateUrl: './landing.html',
+  styleUrl: './landing.css',
+  standalone: true
+})
+export class Landing {
+
+}
