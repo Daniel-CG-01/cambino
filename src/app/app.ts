ce595bb3a065a8f696b51a7b4c5444aff37a0b57
DanielZR05 <danielzanon2005@gmail.com>
2025-10-30 12:01:46 +0100

    Repasar menú.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Landing } from "./components/landing/landing";
+import { Menu } from "./components/menu/menu";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Landing],
+  imports: [RouterOutlet, Landing, Menu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
